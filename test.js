9408d6fd88cc677ff36b140e772d40e5d59895e4
bhupendra tale <118257719+Bhoopendra1035@users.noreply.github.com>
2025-09-25 12:29:47 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,280 @@
+
+// 1.............................................
+
+// let user = prompt("Please enter your age");
+//    if(user>=18){
+
+//        console.log("you are eligible");
+//    }
+//    else{
+//     console.log("you are not")
+//    }
+    
+   
+// 2.................................................
+
+// let check = prompt("enter the number");
+
+// if(check>10 && check<50){
+//     console.log( check  + " is grater then 10 and less then 50 ");
+// }
+// else{
+//     console.log("not full fill the condition ");
+// }
+
+// 3.................................................
+
+// let checkNum = prompt("enter the number");
+
+// if(checkNum<10 && checkNum>50){
+//     console.log( checkNum  + " is less then 10 and grater then 50 ");
+// }
+// else{
+//     console.log("not full fill the condition ");
+    
+// }
+
+//  4.........................................................
+
+// let Num = prompt("enter the number");
+
+// if(Num>0){
+//     console.log("positive");
+// } else if(Num<0){
+//     console.log("negative");
+// } else{
+//     console.log("zero");
+// }
+
+// 5.....................................................................
+ 
+
+//   let divisibleNum = prompt("Enter the number");
+
+//   if((divisibleNum%5==0 )&& (divisibleNum%3==0)){
+//     console.log( divisibleNum  + " is divisible by 5 and 3");
+//   } else{
+//     console.log("not divisible");
+//   }
+
+// 6..............................................................
+
+// let checkChar = prompt("enter the character");
+
+//   if(checkChar == 'a' || checkChar == 'e'|| checkChar=='i'||checkChar=='o'||checkChar=='u'){
+//     console.log(  checkChar  + " is a right character " );
+//   } else{
+//     console.log("wrong character");
+//   }
+  
+// 7............................................................
+
+// let age = prompt("Please enter your age");
+//    if(age>=18){
+
+//        console.log("you can driving");
+//    }
+//    else{
+//     console.log("you are not")
+//    }
+
+
+// 8.....................................................
+
+// let temp = prompt("Enter the temparature");
+   
+// if(temp>20 && temp < 30){
+//     console.log("temp. bw 20 and 30")
+// } else{
+//     console.log(" temp. out of the boundary");
+// }
+
+
+// 9.....................................................
+
+// let day = prompt("enetr the day name");
+
+// if(day=="monday"|| day =="tuesday" ){
+//     console.log(" today is " + day );
+// } else{
+//     console.log("dusra deen he ajj..")
+// }
+
+// 10................................................
+
+// let check_NUm = prompt("Enter the number") ;
+
+//  let res = check_NUm !=10;
+//  console.log(res);
+
+
+// FUNCTION*************
+
+//Q.1: Write a function to calculate factorial of a number.
+//  Q.2: Write a function to find the maximum of two numbers.  
+// Q.3: create a Arrow function and make it any program acoding to you.
+
+//1
+  
+/*function factorial(a){
+    if(a==0||a==1){
+        return 1;
+    }  if (a<0){
+        return "factorial not found";    
+    }
+    return a * factorial(a-1);
+    
+} 
+console.log(factorial(5))*/
+
+//2
+
+/*function myFun(a,b){
+    if(a>b){
+        return a;
+    }
+    if(a==b){
+        return "both equal";
+
+    }
+    return b;
+}
+console.log(myFun(23,23))*/
+
+//3
+
+/*const myFun = (a)=>{
+    if (a<0){
+        return "red";
+    }
+    if(a===0){
+        return "green";
+    }
+    return "blue"
+
+}
+console.log(myFun(-3));*/
+
+
+//Q.1: Check if a number is divisible by 3 and 5. 
+//  Q.2 : Check if a year is a leap year. 
+//  Q.3 Q: Write code using switch to print day of the week.  
+// Q.4 Write a program to check if a number is even or odd. 
+// Q.5 Q: Explain ternary operator with example.
+
+//1 
+
+  /* let divisibleNum = prompt("Enter the number");
+
+  if((divisibleNum%5==0 )&& (divisibleNum%3==0)){
+    console.log( divisibleNum  + " is divisible by 5 and 3");
+  } else{
+    console.log("not divisible");
+  }*/
+
+//2
+    /*const year = prompt("Enter the Year");
+
+    if (year%4==0 && year%100!==0){
+        console.log("year is leap year");
+
+    } else{
+        console.log("not leap year");
+        
+    }*/
+   //3
+
+  /* let day = 4;
+
+   switch (day) {
+    case 1:console.log("sunday");
+     break;
+    case 2:console.log("monday");
+    break;
+    case 3:console.log("tuesday");
+    break;
+    case 4:console.log("thuesday");
+    break;
+    case 5:console.log("wednesday");
+    break;
+    case 6:console.log("friday");
+    break;
+    case 7:console.log("saterday");
+    break;
+   }*/
+
+    //4
+   /* let Num = prompt("enter the number");
+
+      if(Num%2==0){
+       console.log("even");
+    } else {
+        console.log("odd");
+        
+     }*/
+ 
+        //5
+        /*const numCheck = (a)=>(a%2==0?"even":"odd");
+
+        console.log(numCheck(4));*/
+
+        //🔹 Loops
+
+//Q.1: Print numbers from 1 to 10 using a for loop. 
+// Q.2 Write code to print the sum of numbers from 1 to 100.
+//  Q.3 Use for...of to print elements of an array.
+//  Q.4 Q: Write nested loops to print a pattern:
+/*
+
+*
+**
+***
+****
+
+*/  
+
+// 1
+/*let a = 0;
+for(a=0; a<=10; a++){
+    console.log(a);
+
+} 
+*/
+/*let a =1;
+let sum = 0
+
+for( a=1; a<=100;a++){
+     sum = sum + a;
+    console.log(sum);
+}*/
+
+//3
+
+
+    
+
+
+
+
+
+   
+
+
+   
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+  
+  
